eec678957fd1c386262e94485ce05bdff85c4fb3
Nithin Daniel <nithindaniel2018@gmail.com>
2024-04-20 18:34:37 +0530

    get product data

--- routes/api/products.js
+++ routes/api/products.js
@@ -4,23 +4,48 @@ const router = express.Router();
 const Product = require('../../models/products');
 
 
+router.get('/:id', async (req, res) => {
+    const productId = req.params['id'];
 
-router.post('/create',async(req,res)=>{
+    try {
+        const product = await Product.findOne({product_id:productId}).select('-_id -__v')
+        if (!product) {
+            return res.status(404).json({
+                status: 404,
+                message: 'Product not found'
+            });
+        }
 
-    const { product_name,product_qty,product_owner,product_category,seller_id } = req.body;
+        return res.status(200).json({
+            status: 200,
+            message: 'Product found',
+            data: product
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: 'Internal server error'
+        });
+    }
+});
+
+router.post('/create', async (req, res) => {
+
+    const { product_name, product_qty, product_owner, product_category, seller_id } = req.body;
 
     const newProduct = new Product({
-        product_name:product_name,
-        product_qty:product_qty,
-        product_owner:product_owner,
-        product_category:product_category,
-        seller_id:seller_id
+        product_name: product_name,
+        product_qty: product_qty,
+        product_owner: product_owner,
+        product_category: product_category,
+        seller_id: seller_id
     })
     await newProduct.save()
     return res.status(200).json({
         status: 200,
         message: 'Product created successfully'
-    })} );
+    })
+});
 
 
 router.patch('/update/:id', async (req, res) => {
@@ -28,7 +53,7 @@ router.patch('/update/:id', async (req, res) => {
     const { product_name, product_qty, product_owner, product_category, seller_id } = req.body;
 
     try {
-        const existingProduct = await Product.findOne({product_id:productId});
+        const existingProduct = await Product.findOne({ product_id: productId });
         if (!existingProduct) {
             return res.status(404).json({
                 status: 404,
