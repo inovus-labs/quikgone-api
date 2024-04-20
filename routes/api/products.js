3326c9b675f10d4416c9278012cefa77985b81d9
Nithin Daniel <nithindaniel2018@gmail.com>
2024-04-20 18:28:03 +0530

    product update

--- routes/api/products.js
+++ routes/api/products.js
@@ -3,6 +3,8 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../../models/products');
 
+
+
 router.post('/create',async(req,res)=>{
 
     const { product_name,product_qty,product_owner,product_category,seller_id } = req.body;
@@ -20,5 +22,39 @@ router.post('/create',async(req,res)=>{
         message: 'Product created successfully'
     })} );
 
+
+router.patch('/update/:id', async (req, res) => {
+    const productId = req.params['id'];
+    const { product_name, product_qty, product_owner, product_category, seller_id } = req.body;
+
+    try {
+        const existingProduct = await Product.findOne({product_id:productId});
+        if (!existingProduct) {
+            return res.status(404).json({
+                status: 404,
+                message: 'Product not found'
+            });
+        }
+
+        // Update the product fields
+        existingProduct.product_name = product_name;
+        existingProduct.product_qty = product_qty;
+        existingProduct.product_owner = product_owner;
+        existingProduct.product_category = product_category;
+        existingProduct.seller_id = seller_id;
+
+        await existingProduct.save();
+
+        return res.status(200).json({
+            status: 200,
+            message: 'Product updated successfully'
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: 'Internal server error'
+        });
+    }
+});
 
 module.exports = router;
