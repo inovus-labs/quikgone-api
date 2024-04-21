5ef16ea165bcd89293a6a960afdcb19f448f05b4
arjun.airtory <arjun12345krishna@gmail.com>
2024-04-21 12:26:55 +0530

    User Rotes

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,11 +6,13 @@ const router = express.Router();
 const productRouter = require('./products');
 const CartRouter = require('./cart');
 const OrderRouter = require('./orders');
+const UsersRouter = require('./users');
 
 
 router.use('/products', productRouter);
 router.use('/cart', CartRouter);
 router.use('/orders', OrderRouter);
+router.use('/users', UsersRouter);
 
 
 module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,91 @@
+
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/user');
+const verifyToken = require('../../middleware/authentication');
+
+
+
+
+/**
+ * @route   GET /users
+ * @desc    Get all products
+ * @access  Public
+ * @return  message
+ * @error   400, { error }
+ * @status  200, 400
+ * 
+ * @example /products
+**/
+
+router.get('/', async (req, res) => {
+    try {
+
+        const product = await User.find({}).select('-_id -__v')
+        if (!product) {
+            return res.status(404).json({
+                status: 404,
+                message: 'User not found'
+            });
+        }
+
+        return res.status(200).json({
+            status: 200,
+            message: "User found successfully",
+            data: product
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: "Internal server error"
+        });
+    }
+});
+
+
+
+
+/**
+ * @route   GET /users/:id
+ * @desc    Get a single user by id
+ * @access  Public
+ * @return  message
+ * @error   400, { error }
+ * @status  200, 400
+ * 
+ * @example /users/123
+**/
+
+router.get('/:id', async (req, res) => {
+
+    const userId = req.params['id'];
+
+    try {
+        const product = await User.findOne({ user_id: userId }).select('-_id -__v')
+        if (!product) {
+            return res.status(404).json({
+                status: 404,
+                message: 'User not found'
+            });
+        }
+
+        return res.status(200).json({
+            status: 200,
+            message: 'User found',
+            data: product
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: 'Internal server error'
+        });
+    }
+});
+
+
+
+
+
+module.exports = router;
