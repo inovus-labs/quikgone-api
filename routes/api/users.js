904b5b5a6c6671eddec8085de1b62c14fb72cf54
arjun.airtory <arjun12345krishna@gmail.com>
2024-04-21 12:49:44 +0530

    sync

--- routes/api/users.js
+++ routes/api/users.js
@@ -87,5 +87,46 @@ router.get('/:id', verifyToken, async (req, res) => {
 
 
 
+/**
+ * @route   GET /users/me
+ * @desc    Get my user details
+ * @access  Public
+ * @return  message
+ * @error   400, { error }
+ * @status  200, 400
+ * 
+ * @example /users/me
+**/
+
+router.get('/me', verifyToken, async (req, res) => {
+
+    const userId = req.user.user_id;
+
+    try {
+        const product = await User.findOne({ user_id: userId }).select('-_id -__v')
+        if (!product) {
+            return res.status(404).json({
+                status: 404,
+                message: 'User not found'
+            });
+        }
+
+        return res.status(200).json({
+            status: 200,
+            message: 'User found',
+            data: product
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            message: 'Internal server error'
+        });
+    }
+});
+
+
+
+
 
 module.exports = router;
